1e069f64df3588bcb17e9a41983a58fefce0d48f
Your Name <you@example.com>
2023-07-09 12:23:40 +0300

    Init commit

---
+++ src/main/java/b/elections/ElectionManager.java
@@ -0,0 +1,13 @@
+package b.elections;
+
+public class ElectionManager {
+
+    public void printNominees() { ...}
+
+    public void vote(String name, int age, String street, String city) { ...}
+
+    public int countTotalVotes() { ...}
+
+    public String getVoteStats() { ...}
+
+}
